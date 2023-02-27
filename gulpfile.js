4a6dd49e9bb7d69277e236686afe8d882183507c
AnyaSonina <89742951+AnyaSonina@users.noreply.github.com>
2023-02-27 12:05:02 +0000

    css purged

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,12 @@ const sass = require('gulp-sass')(require('sass'))
 function buildStyles() {
   return src('sassfiles/**/*.scss')
   .pipe(sass())
+  .pipe(purgecss({content: ['*.html']}))
   .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['sassfiles/**/*.scss'], buildStyles)
+  watch(['sassfiles/**/*.scss', '*.html'], buildStyles)
 
 }
 
