8e7a735780417c2b69e4631288ca2d6e752e5efd
AnyaSonina <89742951+AnyaSonina@users.noreply.github.com>
2023-02-27 14:53:20 +0000

    f

--- data.js
+++ data.js
@@ -21,3 +21,12 @@ export const menuArray = [
       id: 2
   }
 ]
+
+// "gulp": "^4.0.2",
+// "gulp-build": "^0.5.3",
+// "gulp-gh-pages": "^0.5.4",
+// "gulp-purgecss": "^5.0.0",
+// "gulp-sass": "^5.1.0",
+
+// "gulp-connect": "^5.7.0",
+//     "gulp-open": "^3.0.1",
